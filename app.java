10c400966c775db70d772b33eb758dfe0312d1a8
Veeru463 <mallipudi.veerababu@gmail.com>
2021-08-22 13:04:29 +0530

    Changes for java

---
+++ app.java
@@ -0,0 +1,9 @@
+working with sprint 8.3 user story
+
+working with sprint 8.3
+returnType methodName() {
+  // method body
+}
+modifier static returnType nameOfMethod (parameter1, parameter2, ...) {
+  // method body
+}
